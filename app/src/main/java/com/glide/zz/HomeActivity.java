2cd94214f7cef9b5ed95606312ba78831afb600c
zx <zhangxiong@winchannel.net>
2017-06-14 17:24:47 +0800

    完成glide的配置

--- app/src/main/java/com/glide/zz/HomeActivity.java
+++ app/src/main/java/com/glide/zz/HomeActivity.java
@@ -2,12 +2,20 @@ package com.glide.zz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class HomeActivity extends AppCompatActivity {
+    private ImageView mIvGlide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mIvGlide = (ImageView) findViewById(R.id.miv_for_glide);
+
+        Glide.with(this).load("http://inthecheesefactory.com/uploads/source/nestedfragment/fragments.png").into(mIvGlide);
+
     }
 }
