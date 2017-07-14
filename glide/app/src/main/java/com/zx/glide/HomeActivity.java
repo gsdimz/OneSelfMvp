1250af18d51febc72991bb67190c55ad850764c2
zhangxiong <zhangxiong@winchannel.net>
2017-07-14 14:01:15 +0800

    mvp模式初始化

--- glide/app/src/main/java/com/zx/glide/HomeActivity.java
+++ glide/app/src/main/java/com/zx/glide/HomeActivity.java
@@ -1,13 +1,48 @@
 package com.zx.glide;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
-public class HomeActivity extends Activity {
+import com.zx.glide.mvp.main.MainModel;
+import com.zx.glide.mvp.main.MainPresenter;
+import com.zx.glide.mvp.main.MainView;
+import com.zx.glide.mvp.other.MvpActivity;
 
+import butterknife.ButterKnife;
+
+public class HomeActivity extends MvpActivity<MainPresenter> implements MainView {
+    private ImageView mIvGlide;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        initToolBarAsHome("MVP");
+    }
+
+    @Override
+    protected MainPresenter createPresenter() {
+        return new MainPresenter(this);
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void getDataSuccess(MainModel model) {
+
+    }
+
+    @Override
+    public void getDataFail(String msg) {
+
     }
 }
---
+++ glide/app/src/main/java/com/zx/glide/mvp/main/BaseView.java
@@ -0,0 +1,11 @@
+package com.zx.glide.mvp.main;
+
+/**
+ * Created by zx on 17-7-12.
+ */
+
+public interface BaseView {
+    void showLoading();
+
+    void hideLoading();
+}
